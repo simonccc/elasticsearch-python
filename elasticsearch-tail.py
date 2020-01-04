c4d373a6df33b5ea01312a44d4b22bffcc60d897
simonccc <simonc@gmail.com>
2020-01-04 23:22:45 +0000

    start to add proper color setup and other clean up

--- elasticsearch-tail.py
+++ elasticsearch-tail.py
@@ -6,9 +6,6 @@ from elasticsearch.connection import create_ssl_context
 import warnings
 warnings.filterwarnings("ignore")
 import time as time2
-import threading
-# from random import randint
-import platform
 import re
 import signal
 from elasticsearch import Elasticsearch
@@ -18,12 +15,7 @@ import config as cfg
 def signal_handler(signal, frame):
     sys.exit(0)
 
-def from_epoch_milliseconds_to_string(epoch_milli):
-    return str(datetime.datetime.utcfromtimestamp( float(str( epoch_milli )[:-3]+'.'+str( epoch_milli )[-3:]) ).strftime('%Y-%m-%dT%H:%M:%S.%f'))[:-3]+"Z"
-
-def from_epoch_seconds_to_string(epoch_secs):
-    return from_epoch_milliseconds_to_string(epoch_secs * 1000)
-
+# get the latest doc in the index
 def get_latest_event_timestamp(index):
     res = es.search(size=1, index=index, sort="@timestamp:desc", body={"query": {"match_all": {}} })
 
@@ -32,90 +24,76 @@ def get_latest_event_timestamp(index):
         timestamp = res['hits']['hits'][0]['sort'][0]
         return timestamp
     else:
-        print ("ERROR: get_latest_event_timestamp: No results found in index="+index)
+        print("ERROR: No results found in index="+index)
         sys.exit(1)
 
-# Inserts event into event_pool{}
-def to_object(res):
-  for hit in res['hits']['hits']:
-    message= str(hit['_source']['message'])
-    id = str(hit['_id'])
-    timestamp = str(hit['sort'][0])
-    prog = 'NONE'
-
-    # filebeat
-    if re.search('filebeat', cfg.myindex['name']):
-      host = str(hit['_source']['agent']['hostname'])
-      event_pool[id] = { 'timestamp': timestamp, 'host': host, 'message': message }
-    else:
-      # assume logstash format as default
-      host = str(hit['_source']['logsource'])
-      if (hit['_source']['program']) is not None:
-        prog = str(hit['_source']['program'])
-
-      event_pool[id] = { 'timestamp': timestamp, 'host': host, 'prog': prog,'message': message }
-    return
-
 def search_events(then,now):
-    query_search['query']['bool']['must'] = {"range": {"@timestamp": {"gt": then, "lte": now}}}
-    res = es.search(size="1000", index=index, sort="@timestamp:asc", body=query_search)
+    query = {'query': {'bool': {'must': {'range': {'@timestamp': {'gt': then, 'lte': now }}}}}}
+    res = es.search(size="1000", index=index, sort="@timestamp:asc", body=query)
     return res
 
 def timestamp_short(timestamp):
     short = datetime.datetime.fromtimestamp(int(timestamp) / 1000).strftime('%H:%M:%S.%f')[:-3]
     return short
 
-# Get lastest available index
-def check_index():
+def print_c(color, string):
+  if cfg.tail_colors['enabled'] == 'true': 
+   return(cfg.tail_colors[color] + string  + '\x1b[0m')
+  else:
+   return(string)
 
+# get the index to be tailed
+def get_index():
     indices = []
     list = es.indices.get_alias("*")
+
     for index in list:
+
+        # search for index from config file
         if re.search(str(cfg.myindex['name']), index):
+
+          # if a match is found append it to list
           indices.append(str(index))
+
+    # no match found for supplied index
     if indices == []:
-        print('no index ' + str(cfg.myindex['name']))
+        print('no index found: ' + str(cfg.myindex['name']))
         sys.exit(1)
+
+    # sort the list of indexes
     indices = sorted(indices, reverse=True)
-    print('Tailing \x1b[1;33;91m' + indices[0] + '\x1b[0m')
+
+    # return the latest
     return indices[0]
 
 # Ctrl+C handler
 signal.signal(signal.SIGINT, signal_handler)
 
-#query object base for main search
-query_search = {
-"query": {
-  "bool": {
-    "must": [
-    {
-      "range": {}
-     }
-    ],
-  }
- }
-}
-
 # http://elasticsearch-py.readthedocs.io/en/master/
-ssl_context = create_ssl_context()
-
 # disable these if you don't need them!
+ssl_context = create_ssl_context()
 ssl_context.check_hostname = False
 ssl_context.verify_mode = ssl.CERT_NONE
-
-# and fix this
-es = Elasticsearch(cfg.elastic['es_host'],verify_certs=False,ssl_context=ssl_context,http_auth=(cfg.elastic['user'],cfg.elastic['pass']))
+es = Elasticsearch(
+cfg.elastic['es_host'],
+verify_certs=False,
+ssl_context=ssl_context,
+http_auth=(cfg.elastic['user'],cfg.elastic['pass']))
 
 # get index
-index = check_index()
+index = get_index()
 
 # Get the latest event timestamp from the Index
 latest_event_timestamp = get_latest_event_timestamp(index)
+print('starting from: ' + print_c('red',index  + '-' +  str(timestamp_short(latest_event_timestamp))))
 
 # get current timestamp
 current_time = int(datetime.datetime.utcnow().strftime('%s%f')[:-3])
 
 while True:
+
+  # wait for logs..
+  time2.sleep(cfg.tail['sleep'])
 
   # get latest ES timestamp
   latest_event_timestamp = get_latest_event_timestamp(index)
@@ -128,9 +106,7 @@ while True:
 
     # map dict of results
     for key in results['hits']['hits']:
-
-      message= str(key['_source']['message'])
-#      id = str(key['_id'])
+      message = key['_source']['message']
 
       # timestamp from the last message in the result set is used for the next query
       timestamp = int(key['sort'][0])
@@ -138,14 +114,16 @@ while True:
       # convert timestamp into short time format used in output 
       time = timestamp_short(timestamp)
 
+      # default prog used for logstash
       prog = 'NONE'
 
-      # filebeat
+      # filebeat support
       if re.search('filebeat', cfg.myindex['name']):
         host = str(key['_source']['agent']['hostname'])
       else:
-      # assume logstash format as default
         host = str(key['_source']['logsource'])
+
+        # prog
         if (key['_source']['program']) is not None:
           prog = str(key['_source']['program'])
 
@@ -153,5 +131,3 @@ while True:
 
     # end of results so set "current" timestamp to the last result
     current_time = timestamp
-
-  time2.sleep(0.2)
--- example-config.py
+++ example-config.py
@@ -3,3 +3,10 @@ elastic = {'es_host': 'https://localhost:9200',
 	 'pass': 'admin'}
 #logstash and filebeat tested
 myindex = {'name': 'logstash'}
+tail = {
+        'sleep': 0.2
+}
+tail_colors = {
+        'enabled': 'true',
+        'red': '\x1b[1;33;91m'
+}
