e1fc9d9babe8cf727a41af091c0538c86ec0f9f7
Simon Chrich <simonc@gmail.com>
2020-01-05 13:55:20 +0000

    fix example config

--- example-config.py
+++ example-config.py
@@ -9,7 +9,7 @@ tail = {
 }
 tail_colors = {
         'enabled': 'true',
-        'red': '\x1b[1;33;91m'
+        'red': '\x1b[1;33;91m',
         'blue': '\x1b[1;33;94m',
         'yellow': '\x1b[1;33;33m',
         'green': '\x1b[1;33;92m',
